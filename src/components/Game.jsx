af935e6f89f8d225d75d1a54c50a0f81cb524c80
FaseehFS <faseeh1080@gmail.com>
2025-12-07 14:45:10 +0530

    Generate questions and answers

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,13 +1,40 @@
 import * as React from "react";
 
 export default function Game({ setScene, gameMode }) {
+  var generateQAndA;
+
+  if (gameMode === "times-table") {
+    generateQAndA = () => {
+      const num1 = Math.floor(Math.random() * 10);
+      const num2 = Math.floor(Math.random() * 10);
+      const question = `${num1} × ${num2}`;
+      const answer = num1 * num2;
+      return [question, answer];
+    };
+  } else if (gameMode === "addition") {
+    generateQAndA = () => {
+      const num1 = Math.floor(Math.random() * 91);
+      const num2 = Math.floor(Math.random() * 10);
+      const question = `${num1} + ${num2}`;
+      const answer = num1 + num2;
+      return [question, answer];
+    };
+  }
+
+  var [currentQa, setCurrentQa] = React.useState(generateQAndA());
+
+  function updateQa() {
+    setCurrentQa(generateQAndA());
+  }
+
   function goBack() {
     setScene("menu");
   }
 
   return (
     <div>
-      <h1>Game Mode: {gameMode}</h1>
+      <h1>{currentQa[0]}</h1>
+      <button onClick={updateQa}>New</button>
       <button onClick={goBack}>Go Back</button>
     </div>
   );
