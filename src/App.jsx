056fd48e99552d55737d015b90dfec790c48c4d4
FaseehFS <faseeh1080@gmail.com>
2025-12-07 08:37:21 +0530

    Create Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 
+function Menu() {
+  return (
+    <div className="vbox" style={{ width: "16rem" }}>
+      <button>Times Table</button>
+      <button>Addition</button>
+      <button>Both</button>
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
       <h1 style={{ margin: "0 0 2rem 0" }}>Let's Start Training!</h1>
-      <div class="vbox" style={{ width: "16rem" }}>
-        <button>Times Table</button>
-        <button>Addition</button>
-        <button>Both</button>
-      </div>
+      <Menu></Menu>
     </>
   );
 }
